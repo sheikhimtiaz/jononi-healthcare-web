25c2d121c709f3d8227e904ed0c79e6766c20732
sheikhimtiaz <imticorei54@gmail.com>
2020-05-15 22:26:50 -0700

    name fixed

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to mdb-angular-boilerplate!');
+    expect(page.getParagraphText()).toEqual('Welcome to jononi-assistant!');
   });
 });
